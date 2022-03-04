90086fbba5c4f86d7d9cb4abff71c50814391934
channmixxx <2405693220@qq.com>
2022-03-04 23:57:59 +0800

    京东狗

--- jingdongdog.js
+++ jingdongdog.js
@@ -1,12 +1,8 @@
 const logo = document.querySelector('.logo');
-// 鼠标放置事件
-logo.addEventListener('mouseover', function() {
-  // 处理放置事件
-  logo.src = './jd2.png';
-});
 
-// 鼠标离开事件
+logo.addEventListener('mouseover', function() {
+  logo.src = './images/jd2.png';
+});
 logo.addEventListener('mouseout', function() {
-  // 处理离开事件
-  logo.src = './jd1.png';
+  logo.src = './images/jd1.png';
 });
